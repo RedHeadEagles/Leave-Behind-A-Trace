c83981a69c365f908bfdb6857ffbeb8fecd326e7
William <redheadeagles@gmail.com>
2021-08-05 18:56:27 -0600

    AI for the ships in the Pirate level

---
+++ Leave-Behind-A-Trace/Assets/Scripts/LShipAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LShipAI : MonoBehaviour
+{
+    public int health;
+    public GameObject cannonball;
+    private Rigidbody2D rb2d;
+    private int timer = 0;
+    public GameObject mast;
+    private bool movingRight = false;
+    private float xMin = -8.46f;
+    private float xMax = 8.46f;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health == 0)
+        {
+            Instantiate(mast, transform.localPosition - new Vector3(0, 2), Quaternion.identity);
+        }
+        if (timer % 75 == 0)
+        {
+            if (Random.Range(0, 100000000) % 20 == 0)
+            {
+                timer++;
+                Instantiate(cannonball, transform.localPosition - new Vector3(0, 1.23f), Quaternion.identity);
+            }
+        }
+        else
+        {
+            timer++;
+        }
+        if(movingRight && transform.localPosition.x < xMax)
+        {
+            transform.localPosition += new Vector3(speed * Time.deltaTime, 0);
+            movingRight = transform.localPosition.x < xMax;
+        }
+        else
+        {
+            transform.localPosition -= new Vector3(speed * Time.deltaTime, 0);
+            movingRight = !(transform.localPosition.x > xMin);
+        }
+    }
+}
---
+++ Leave-Behind-A-Trace/Assets/Scripts/RightLShip.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightLShip : MonoBehaviour
+{
+    public GameObject cannonball;
+    private int timer = 0;
+    private bool movingUp = false;
+    private float yMin = -2.09f;
+    private float yMax = 3.65f;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer % 75 == 0)
+        {
+            if (Random.Range(0, 100000000) % 20 == 0)
+            {
+                timer++;
+                Instantiate(cannonball, transform.localPosition - new Vector3(1.23f, 0), Quaternion.identity);
+            }
+        }
+        else
+        {
+            timer++;
+        }
+        if (movingUp && transform.localPosition.y < yMax)
+        {
+            transform.localPosition += new Vector3(0, speed * Time.deltaTime);
+            movingUp = transform.localPosition.y < yMax;
+        }
+        else
+        {
+            transform.localPosition -= new Vector3(0, speed * Time.deltaTime);
+            movingUp = !(transform.localPosition.y > yMin);
+        }
+    }
+}
